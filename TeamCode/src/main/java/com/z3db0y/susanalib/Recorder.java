4d35e049546d869f893640dae77fc4a9f693ac16
z3db0y <us3r0002@gmail.com>
2022-06-26 21:35:18 +0300

    A bit of recorder :P

--- TeamCode/src/main/java/com/z3db0y/susanalib/Recorder.java
+++ TeamCode/src/main/java/com/z3db0y/susanalib/Recorder.java
@@ -19,10 +19,31 @@ public class Recorder {
     }
 
     public void start() {
-        if(recordingThread != null) {
-            recordingThread.interrupt();
-        }
+        if(recordingThread != null) recordingThread.interrupt();
         recording.clear();
+        recordingThread = new Thread(() -> {
+            while(!Thread.interrupted()) {
+                double prevTime = 0;
+                StringBuilder recLine = new StringBuilder();
+                for(String name : motors.keySet()) {
+                    recLine.append(name);
+                    recLine.append("=");
+                    recLine.append(motors.get(name).getCurrentPosition());
+                    recLine.append("|");
+                    recLine.append(motors.get(name).getPower());
+                    recLine.append("&");
+                }
+                recLine.deleteCharAt(recLine.length() - 1);
+                recording.add(recLine.toString());
+                while(System.currentTimeMillis() < prevTime + 100) {}
+            }
+        });
+        recordingThread.start();
     }
 
+    public void stopAndSave(String filename) {
+        if(recordingThread != null) recordingThread.interrupt();
+    }
+
+    public void replay(String filename) {}
 }
